5c1081d01fa58828b30c60af0532e29a7dcffd89
Fred Emmott <fe@fb.com>
2018-07-19 13:50:39 -0700

    put code in soruce

--- index.js
+++ index.js
@@ -11,6 +11,15 @@
 const { AutoLanguageClient } = require('atom-languageclient');
 const fs = require('fs');
 const cp = require('child_process');
+const LinterAdapter = require('atom-languageclient/build/lib/adapters/linter-push-v2-adapter').default;
+
+// Monkey patching time: atom-ide-ui and nuclide don't show the code by default. Put it in `source` to be
+// consistent with nuclide-hack
+const originalHandler = LinterAdapter.prototype.diagnosticToV2Message;
+LinterAdapter.prototype.diagnosticToV2Message = (path, diagnostic) => {
+  diagnostic.source = diagnostic.source + ": " + diagnostic.code;
+  return originalHandler(path, diagnostic);
+}
 
 class HHASTLanguageClient extends AutoLanguageClient {
   getGrammarScopes() { return ['source.hack', 'text.html.hack']; }
